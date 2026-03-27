c66880457cd3ac07cf9389545ddb7bdf4ed66080
unknown <sanjanayadav@gmail.com>
2026-03-27 20:00:14 +0530

    Fixed port binding for Render

--- backend/app.py
+++ backend/app.py
@@ -27,5 +27,12 @@ def get_stock(symbol):
         "decision": decision
     })
 
+    import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    print("SERVER STARTING...")
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=False
+    )
